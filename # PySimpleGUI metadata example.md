3b41b4def9dc0236e0f4e1f9aed31bb16be24f9b
Varigarble <58060789+Varigarble@users.noreply.github.com>
2020-11-18 18:30:48 -0800

    Add files via upload

---
+++ # PySimpleGUI metadata example.md
@@ -0,0 +1,13 @@
+# PySimpleGUI metadata example
+
+#### What it is:
+
+This is an example of how to display a portion of data in a PySimplGUI Listbox while effecting changes to a larger portion of data, and also an example of how to use the metadata parameter. 
+
+#### How it works:
+
+The enumerated namedtuple class list (indexed_list) approximates a data type that one might compose in retrieving rows from a database. The enumeration (e.g., a database row id) and product SKUs are not displayed in the Listbox gui. When a selection is made in the 'Shelf' Listbox, the metadata is updated with the indexes of the highlighted items. The final output is copied from the indexed_list by retrieving the items that are at the indexes corresponding to those values collected in the metadata parameter.
+
+#### Why do it this way:
+
+The Listbox returns an ordinary Python list class. It is possible to compare the displayed items to larger pieces of data that contain the displayed items, but this can lead to false matches if the displayed data is not unique enough. For example, the organic banana at the end of the list has the same name and price as the non-organic banana. If we search the indexed_list for the item that matches the name and price, Python will stop at the first match, which would be the non-organic banana. 
---
+++ pysimplegui_hidden_metadata.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+import PySimpleGUI as sg
+
+sg.theme('Dark Blue 3')
+
+Produce = namedtuple('Produce', ['Name', 'SKU', 'Price'])
+
+banana = Produce('Banana', 4419, 0.89)
+organic_banana = Produce('Banana', 99419, 0.89)  # this is a copy of banana with a different SKU only
+apple = Produce('Gala Apple', 4012, 0.69)
+beans = Produce('Black Beans', 7559, 1.29)
+soy_cake = Produce('Tofu', 88682, 4.25)
+
+shelf = [banana, apple, beans, soy_cake, apple, apple, organic_banana]
+# the "shelf" list index is hardcoded to the items so that we can see the exact items that are placed in the bag:
+indexed_shelf = list(enumerate(shelf))
+gui_shelf = [(item[0], item[2]) for item in shelf]  # a subset of data for a pretty display
+
+bag = []  # empty bag to fill at checkout
+
+layout = [[sg.T("Shelf", size=(37, 0), justification='left'), sg.T("Basket", size=(37, 0), justification='left')],
+        [sg.Listbox(values=gui_shelf, size=(40, 10), select_mode='multiple', key='SELECTION', enable_events=True, metadata=()),
+        sg.Listbox(values=['Empty Basket'], size=(40, 10), key='BASKET')],
+        [sg.B("View Metadata", key='-META-'), sg.B("Checkout", key='CHECKOUT')]
+        ]
+
+window = sg.Window(layout=layout, title='Go shopping!', element_padding=((10, 10), (5, 5)))
+
+while True:
+    event, value = window.read()
+    
+    if event is None:
+        window.close()
+        break
+
+    if event == 'SELECTION':
+        # replace existing metadata with a tuple of the indexes of highlighted items in the Listbox:
+        window.Element('SELECTION').metadata=window.Element('SELECTION').GetIndexes()
+        # copy the pretty display of items in a second Listbox:
+        window.Element('BASKET').Update(value['SELECTION'])
+
+    if event == '-META-':
+        print("metadata: ", window.Element('SELECTION').metadata)
+
+    if event == 'CHECKOUT':
+        for item in indexed_shelf:
+            if indexed_shelf.index(item) in window.Element('SELECTION').metadata:
+                bag.append(item)
+        print("metadata: ", window.Element('SELECTION').metadata)
+        print("bag: ", bag)
+        bag = []  # get a new empty bag
